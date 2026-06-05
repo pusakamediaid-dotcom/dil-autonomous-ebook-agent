4839bcf3b550c519b7d7268026cc31677013cc32
Pusaka-Media.ID <pusaka.media.id@gmail.com>
2026-06-05 15:12:02 +0700

    Blok 1.6: perbaiki logger core

--- src/core/logger.py
+++ src/core/logger.py
@@ -22,24 +22,4 @@ import re
 
 
 class MaskingFilter(logging.Filter):
-    """Filter untuk menyensor string panjang yang terlihat seperti secret."""
-
-    PATTERNS_TO_MASK = [r"[A-Za-z0-9_\-]{32,}"]
-
-    def filter(self, record):
-        record.msg = self._mask_secrets(str(record.msg))
-        if record.args:
-            record.args = tuple(
-                self._mask_secrets(str(arg)) if isinstance(arg, str) else arg
-                for arg in (record.args if isinstance(record.args, tuple) else (record.args,))
-            )
-        return True
-
-    def _mask_secrets(self, text: str) -> str:
-        for pattern in self.PATTERNS_TO_MASK:
-            def mask_match(match):
-                value = match.group(0)
-                if len(value) >= 32:
-                    return value[:4] + "*" * (len(value) - 4)
-                return value
-            text = re.sub
+    """
